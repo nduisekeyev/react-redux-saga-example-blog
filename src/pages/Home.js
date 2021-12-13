4d075dda64aad2cd5cc1f568d592c084d1e7253f
Nurmukhammed Duisekeyev <nureke92@gmail.com>
2021-12-13 15:51:04 +0600

    feat: show the posts

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+// src/pages/Home.js
+
+import { Container } from "react-bootstrap";
+import Posts from "../components/Posts";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPosts } from "../store/posts/actions";
+
+export default function Home() {
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  return (
+    <Container className="home">
+      <Posts />
+    </Container>
+  );
+}

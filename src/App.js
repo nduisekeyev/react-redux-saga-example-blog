4ca4de9e93aea15632a09f90b1c4890f67fbef18
Nurmukhammed Duisekeyev <nureke92@gmail.com>
2021-12-13 15:07:39 +0600

    fix: remove Router, use only one Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import Home from "./pages/Home";
 import Singlepost from "./pages/SinglePost";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" exact component={() => <Home />} />
-          <Route path="/:id" exact component={() => <Singlepost />} />
-        </Routes>
-      </Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" exact component={() => <Home />} />
+        <Route path="/:id" exact component={() => <Singlepost />} />
+      </Routes>
     </div>
   );
 }

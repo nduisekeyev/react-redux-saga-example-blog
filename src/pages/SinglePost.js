a0020b99c943c5c44c11adc1e846d62a4b516c98
Nurmukhammed Duisekeyev <nureke92@gmail.com>
2021-12-13 15:51:33 +0600

    feat: show the single post

---
+++ src/pages/SinglePost.js
@@ -0,0 +1,24 @@
+// src/pages/SinglePost.js
+
+import { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import PostDetails from "../components/PostDetails";
+import { useDispatch } from "react-redux";
+import { getPostDetails } from "../store/posts/actions";
+import { useParams } from "react-router-dom";
+
+function SinglePost() {
+  let params = useParams();
+  let dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPostDetails(params.id));
+  }, [dispatch, params.id]);
+
+  return (
+    <Container className="single-post">
+      <PostDetails />
+    </Container>
+  );
+}
+export default SinglePost;
